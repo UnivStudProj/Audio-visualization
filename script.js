575c58195fbe5f83880dbbbc1172cf9a66b9968d
muivproject <m70155302@yandex.ru>
2022-03-20 16:31:46 +0300

    rectangles

--- script.js
+++ script.js
@@ -1,39 +1,85 @@
-var audio, context, analyser, src, array, logo;
+var context, analyser, src, array;
 
-logo = document.getElementById('logo').style;
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+// const logo = document.getElementById('logo').style;
+const audio = document.querySelector('audio');
 
-audio = document.querySelector('audio');
+// window.onclick = function() {
+//     if (!context) {
+//         preparation();
+//     }
 
-window.onclick = function() {
-    if (!context) {
-        preparation();
+//     if (audio.paused) {
+//         audio.play();
+//         loop();
+//     } else {
+//         audio.pause();
+//     }
+// }
+var w = canvas.width = window.innerWidth;
+var h = canvas.height = window.innerHeight;
+
+window.onresize = function() {
+    w = canvas.width = window.innerWidth;
+    h = canvas.height = window.innerHeight;
+}
+
+const rectangles = new Array(50);
+const offsideIndent = 20;       // indent from the screen sides
+const insideIndent = 3;         // indent between rectangles
+const property = {
+    rMinHeight     : 10,
+    rMaxHeight     : 100,
+    rWidth         : (w - offsideIndent) / rectangles.length,
+    rColor         : 'hsl(300, 100%, 95%)',
+    rCount         : rectangles.length
+}
+
+
+class Rectangle {
+    constructor(x, y, height, i) {
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.i = i;
     }
 
-    if (audio.paused) {
-        audio.play();
-        loop();
-    } else {
-        audio.pause();
+    rDraw() {
+        ctx.fillStyle = property.rColor;
+        if (this.i == rectangles.length - 1) ctx.fillStyle = 'red';
+        ctx.fillRect(this.x, this.y, property.rWidth - insideIndent, this.height);
     }
 }
 
-function preparation() {
-    context = new AudioContext();
-    analyser = context.createAnalyser();
-    src = context.createMediaElementSource(audio);
-    src.connect(analyser);
-    analyser.connect(context.destination);
-    loop();
+var indent = offsideIndent / 2 + insideIndent / 2;
+for (let i = 0; i < property.rCount; i++) {
+    let x = indent;
+    let y = h / 2;
+    let rect = new Rectangle(x, y, property.rMinHeight, i);
+    rectangles[i] = rect;
+    indent += property.rWidth;
 }
 
-function loop() {
-    if (!audio.paused) {
-        window.requestAnimationFrame(loop);
-    }
+rectangles.forEach(rect => rect.rDraw());
 
-    array = new Uint8Array(analyser.frequencyBinCount);
-    analyser.getByteFrequencyData(array);
+// function preparation() {
+//     context = new AudioContext();
+//     analyser = context.createAnalyser();
+//     src = context.createMediaElementSource(audio);
+//     src.connect(analyser);
+//     analyser.connect(context.destination);
+//     loop();
+// }
 
-    logo.minHeight = (array[40]) + 'px';
-    logo.width = (array[40]) + 'px';
-}
+// function loop() {
+//     if (!audio.paused) {
+//         window.requestAnimationFrame(loop);
+//     }
+
+//     array = new Uint8Array(analyser.frequencyBinCount);
+//     analyser.getByteFrequencyData(array);
+
+//     logo.minHeight = (array[40]) + 'px';
+//     logo.width = (array[40]) + 'px';
+// }
