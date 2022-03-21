451624c1553f444f4914f4568f0979370380dc23
muivproject <m70155302@yandex.ru>
2022-03-21 14:21:59 +0300

    rework

--- script.js
+++ script.js
@@ -1,85 +1,48 @@
 var context, analyser, src, array;
 
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
-// const logo = document.getElementById('logo').style;
+const container = document.getElementById('container');
 const audio = document.querySelector('audio');
 
-// window.onclick = function() {
-//     if (!context) {
-//         preparation();
-//     }
-
-//     if (audio.paused) {
-//         audio.play();
-//         loop();
-//     } else {
-//         audio.pause();
-//     }
-// }
-var w = canvas.width = window.innerWidth;
-var h = canvas.height = window.innerHeight;
-
-window.onresize = function() {
-    w = canvas.width = window.innerWidth;
-    h = canvas.height = window.innerHeight;
-}
-
-const rectangles = new Array(50);
-const offsideIndent = 20;       // indent from the screen sides
-const insideIndent = 3;         // indent between rectangles
-const property = {
-    rMinHeight     : 10,
-    rMaxHeight     : 100,
-    rWidth         : (w - offsideIndent) / rectangles.length,
-    rColor         : 'hsl(300, 100%, 95%)',
-    rCount         : rectangles.length
-}
-
-
-class Rectangle {
-    constructor(x, y, height, i) {
-        this.x = x;
-        this.y = y;
-        this.height = height;
-        this.i = i;
+window.onclick = function() {
+    if (!context) {
+        preparation();
     }
 
-    rDraw() {
-        ctx.fillStyle = property.rColor;
-        if (this.i == rectangles.length - 1) ctx.fillStyle = 'red';
-        ctx.fillRect(this.x, this.y, property.rWidth - insideIndent, this.height);
+    if (audio.paused) {
+        audio.play();
+        loop();
+    } else {
+        audio.pause();
     }
 }
 
-var indent = offsideIndent / 2 + insideIndent / 2;
-for (let i = 0; i < property.rCount; i++) {
-    let x = indent;
-    let y = h / 2;
-    let rect = new Rectangle(x, y, property.rMinHeight, i);
-    rectangles[i] = rect;
-    indent += property.rWidth;
+const rectangles = new Array(60);
+
+for (let i = 0; i < rectangles.length; i++) {
+    let rect = document.createElement('div');
+    rect.className = 'rectangle';
+    container.appendChild(rect);
 }
 
-rectangles.forEach(rect => rect.rDraw());
 
-// function preparation() {
-//     context = new AudioContext();
-//     analyser = context.createAnalyser();
-//     src = context.createMediaElementSource(audio);
-//     src.connect(analyser);
-//     analyser.connect(context.destination);
-//     loop();
-// }
+function preparation() {
+    context = new AudioContext();
+    analyser = context.createAnalyser();
+    src = context.createMediaElementSource(audio);
+    src.connect(analyser);
+    analyser.connect(context.destination);
+    loop();
+}
 
-// function loop() {
-//     if (!audio.paused) {
-//         window.requestAnimationFrame(loop);
-//     }
+function loop() {
+    if (!audio.paused) {
+        window.requestAnimationFrame(loop);
+    }
 
-//     array = new Uint8Array(analyser.frequencyBinCount);
-//     analyser.getByteFrequencyData(array);
+    array = new Uint8Array(analyser.frequencyBinCount);
+    analyser.getByteFrequencyData(array);
 
-//     logo.minHeight = (array[40]) + 'px';
-//     logo.width = (array[40]) + 'px';
-// }
+    for (let i = 0; i < rectangles.length; i++) {
+        rectangles[i].style.height = array[i];
+    }
+}
